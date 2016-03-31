42d53961ee9ad9e3a62ea2880f1ee84463256d4d
Shawn <sdgu000@gmail.com>
2016-03-31 02:48:42 -0400

    megas use the base abilities

--- public/javascripts/app.controller.roomCtrl.js
+++ public/javascripts/app.controller.roomCtrl.js
@@ -1,5 +1,40 @@
 'use strict';
 
+
+angular.module("collabteambuilder").directive('optionsClass', function ($parse) {
+  return {
+    require: 'select',
+    link: function(scope, elem, attrs, ngSelect) {
+      // get the source for the items array that populates the select.
+      var optionsSourceStr = attrs.ngOptions.split(' ').pop(),
+      // use $parse to get a function from the options-class attribute
+      // that you can use to evaluate later.
+          getOptionsClass = $parse(attrs.optionsClass);
+          
+      scope.$watch(optionsSourceStr, function(items) {
+        // when the options source changes loop through its items.
+        angular.forEach(items, function(item, index) {
+          // evaluate against the item to get a mapping object for
+          // for your classes.
+          var classes = getOptionsClass(item),
+          // also get the option you're going to need. This can be found
+          // by looking for the option with the appropriate index in the
+          // value attribute.
+              option = elem.find('option[value=' + index + ']');
+              
+          // now loop through the key/value pairs in the mapping object
+          // and apply the classes that evaluated to be truthy.
+          angular.forEach(classes, function(add, className) {
+            if(add) {
+              angular.element(option).addClass(className);
+            }
+          });
+        });
+      });
+    }
+  };
+});
+
 angular.module("collabteambuilder").controller("RoomCtrl", function($scope, rooms, post, dex)
 {
 
@@ -145,6 +180,12 @@ angular.module("collabteambuilder").controller("RoomCtrl", function($scope, room
 		{
 			return {'background-color' : 'white'};
 		}
+	}
+
+
+	$scope.EVStyle = function(n)
+	{
+		if (n > 100) return {'background-color' : 'green'}
 	}
 
 
@@ -704,6 +745,12 @@ angular.module("collabteambuilder").controller("RoomCtrl", function($scope, room
 	$scope.getAbs = function(num)
 	{
 		var mon = $scope.party["pokemon" + num].name;
+		var dexE = getDexEntry(mon);
+		if (dexE.baseSpecies)
+		{
+			mon = dexE.baseSpecies;
+		}
+
 		var abs = [];
 		if (pokedex) 
 		{
